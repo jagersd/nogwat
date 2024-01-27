f465cf52c3b80a6fb8a7838a7731f33e2f2865ad
jagersd <jagersd@gmail.com>
2024-01-27 15:06:57 +0100

    added forget-me reference to privacy policy

--- nogwat-api/resources/views/privacy-policy.blade.php
+++ nogwat-api/resources/views/privacy-policy.blade.php
@@ -9,50 +9,50 @@
     </head>
     <body>
         <div class="policy-container">
-            <h1>Privacy Policy</h1> 
+            <h1>Privacy Policy</h1>
             <p>
                 The NogWat-App app is built and intended as
                 a Free app. This SERVICE is provided by
                 S de Jager at no cost and is intended for use as
                 is.
-            </p> 
+            </p>
             <p>
                 This page is used to inform visitors regarding the
                 policies with the collection, use, and disclosure of Personal
                 Information if anyone decided to use this Service.
-            </p> 
+            </p>
             <p>
                 If you choose to use the NogWat app, then you agree to
                 the collection and use of information in relation to this
                 policy. The Personal Information collected is
                 used for authentication only. Your information will not be shared except as described in this Privacy Policy.
-            </p> 
+            </p>
             <p>
                 The terms used in this Privacy Policy have the same meanings
                 as in our Terms and Conditions, which are accessible at
                 NogWat-App unless otherwise defined in this Privacy Policy.
-            </p> 
-            <p><strong>Information Collection and Use</strong></p> 
+            </p>
+            <p><strong>Information Collection and Use</strong></p>
             <p>
                 For access to all features of this application, while using the Service, you
                 may be required to provide certain personally
                 identifiable information, including but not limited to email address. The information requested will be retained
-                on your device and the application's centralised backend. 
-            </p> 
+                on your device and the application's centralised backend.
+            </p>
             <div>
                 <p>
                     The app does use third-party services that may collect
                     information used to identify you.
-                </p> 
+                </p>
                 <p>
                     Link to the privacy policy of third-party service providers used
                     by the app
-                </p> 
+                </p>
                 <ul>
                     <li><a href="https://www.google.com/policies/privacy/" target="_blank" rel="noopener noreferrer">Google Play Services</a></li>
                 </ul>
-            </div> 
-            <p><strong>Log Data</strong></p> 
+            </div>
+            <p><strong>Log Data</strong></p>
             <p>
                 We want to inform you that whenever you
                 use the Service, in a case of an error in the app
@@ -63,14 +63,14 @@
                 configuration of the app when utilizing my Service,
                 the time and date of your use of the Service, and other
                 statistics.
-            </p> 
-            <p><strong>Cookies</strong></p> 
+            </p>
+            <p><strong>Cookies</strong></p>
             <p>
                 Cookies are files with a small amount of data that are
                 commonly used as anonymous unique identifiers. These are sent
                 to your browser from the websites that you visit and are
                 stored on your device's internal memory.
-            </p> 
+            </p>
             <p>
                 This Service does not use these “cookies” explicitly. However,
                 the app may use third-party code and libraries that use
@@ -79,27 +79,27 @@
                 and know when a cookie is being sent to your device. If you
                 choose to refuse our cookies, you may not be able to use some
                 portions of this Service.
-            </p> 
-            <p><strong>Service Providers</strong></p> 
+            </p>
+            <p><strong>Service Providers</strong></p>
             <p>
                 Third-party companies and
                 individuals may be employed exclusively for the following reasons:
-            </p> 
+            </p>
             <ul>
-                <li>To facilitate our Service;</li> 
-                <li>To provide the Service on our behalf;</li> 
-                <li>To perform Service-related services; or</li> 
+                <li>To facilitate our Service;</li>
+                <li>To provide the Service on our behalf;</li>
+                <li>To perform Service-related services; or</li>
                 <li>To assist us in analyzing how our Service is used.</li>
                 <li>To perform data security and integrity tests.</li>
-            </ul> 
+            </ul>
             <p>
                 We want to inform users of this Service
                 that these third parties have access to their Personal
                 Information. The reason is to perform the tasks assigned to
                 them on our behalf. However, they are obligated not to
                 disclose or use the information for any other purpose.
-            </p> 
-            <p><strong>Security</strong></p> 
+            </p>
+            <p><strong>Security</strong></p>
             <p>
                 We value your trust in providing us your
                 Personal Information, thus we are striving to use commercially
@@ -107,8 +107,10 @@
                 of transmission over the internet, or method of electronic
                 storage is 100% secure and reliable, and we cannot
                 guarantee its absolute security.
-            </p> 
-            <p><strong>Links to Other Sites</strong></p> 
+                Should you require us to remove your data from the NogWat App,
+                Please submit your request <a scr="https://nogwat-app.nl/forget-me">here</a>.
+            </p>
+            <p><strong>Links to Other Sites</strong></p>
             <p>
                 This Service may contain links to other sites. If you click on
                 a third-party link, you will be directed to that site. Note
@@ -118,8 +120,8 @@
                 no control over and assume no responsibility for the content,
                 privacy policies, or practices of any third-party sites or
                 services.
-            </p> 
-            <p><strong>Children's Privacy</strong></p> 
+            </p>
+            <p><strong>Children's Privacy</strong></p>
             <div>
                 <p>
                     These Services do not address anyone under the age of 13.
@@ -133,16 +135,17 @@
                     we will be able to do the necessary actions.
                 </p>
             </div>
-            <p><strong>Changes to This Privacy Policy</strong></p> 
+            <p><strong>Changes to This Privacy Policy</strong></p>
             <p>
                 We may update our Privacy Policy from
                 time to time. Thus, you are advised to review this page
                 periodically for any changes. We will
                 notify you of any changes by posting the new Privacy Policy on
                 this page.
-            </p> 
-            <i>This policy is effective as of 2022-03-30</i> 
-            <p><strong>Contact Us</strong></p> 
+            </p>
+            <i>This policy is effective as of 2022-03-30</i>
+            <i>2024-01-27: Reference to data deletion request added.</i>
+            <p><strong>Contact Us</strong></p>
             <p>
                 If you have any questions or suggestions about my
                 Privacy Policy, do not hesitate to contact me at nogwatapp@gmail.com.
